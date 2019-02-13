d3e33205cf50738ff16d027df1fd24e5269bebc8
AHETESHAMS <aheteshams007@gmail.com>
2019-02-13 19:08:22 +0530

    late evening Changes

--- JAVAp/src/com/BridgeLabz/FunctionalPrograms/Utility.java
+++ JAVAp/src/com/BridgeLabz/FunctionalPrograms/Utility.java
@@ -45,11 +45,107 @@ public class Utility {
 	}
 	
 	/**
+	 * 
+	 * @param arr
+	 */
+	public static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+	
+	/**
+	 * Function: Sorting for merge sort
+	 * @param arr
+	 * @param l
+	 * @param r
+	 */
+	public static void sort(int arr[], int l, int r) 
+    { 
+        if (l < r) 
+        { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+            sort(arr, l, m); 
+            sort(arr , m+1, r); 
+  
+            // Merge the sorted
+            merge(arr, l, m, r); 
+        } 
+    } 
+	/**
+	 * Function: Merging For Merge Sort
+	 * @param arr
+	 * @param l
+	 * @param m
+	 * @param r
+	 */
+	
+	public static void merge(int arr[], int l, int m, int r) 
+    { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = arr[m + 1+ j]; 
+  
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) 
+        { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+	
+	/**
 	 * Function:To convert decimal To binary
 	 * @param binary:Integer Array
 	 * @param num: Integer Number
 	 * @return: Integer
 	 */
+	
 	public static int integerToBinary(int []binary,int num)
 	{
 		int i=0,t;
